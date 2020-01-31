1087d4993d5d2867f8c71f76a245b519b6951182
Get <dkdlwmzhs@naver.com>
2020-02-01 00:20:49 +0900

    123

--- index_Html.js
+++ index_Html.js
@@ -1,3 +1,4 @@
-const cssTitle = document.getElementById("title");
+const cssTitle = document.getElementById("title"); // title 태그 객체 가져옴
+cssTitle.innerHTML = "Hi! From JS"; // HTML에 title객체 바꾸기
 
-console.log(title);
+console.dir(cssTitle); // DOM객체 title의 함수들을 콘솔로 출력 해보기
